568ef68e2a0464e2dc52ab08fddf341293c2d9bb
gdulik <tomas.gdula@gmail.com>
2021-04-17 11:26:23 +0200

    Added 10-songs

---
+++ 10-songs/src/components/SongList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { selectSong } from '../actions';
+
+function SongList({ songs, selectSong }) {
+	const renderList = () => {
+		return songs.map((song) => (
+			<div className="item" key={song.title}>
+				<div className="right floated content">
+					<button
+						className="ui button primary"
+						onClick={() => selectSong(song)}
+					>
+						Select
+					</button>
+				</div>
+				<div className="content">{song.title}</div>
+			</div>
+		));
+	};
+	return <div className="ui divided list">{renderList()}</div>;
+}
+
+const mapStateToProps = (state) => ({ songs: state.songs });
+
+export default connect(mapStateToProps, { selectSong })(SongList);
