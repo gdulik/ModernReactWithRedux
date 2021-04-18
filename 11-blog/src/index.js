5490380504687afc2eb13b0a9311a5d0ca853a52
gdulik <tomas.gdula@gmail.com>
2021-04-18 20:15:21 +0200

    Added 11-blog

---
+++ 11-blog/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import App from './components/App';
+import reducers from './reducers';
+
+const store = compose(
+	applyMiddleware(thunk),
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)(createStore)(reducers);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
