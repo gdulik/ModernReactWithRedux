5b59b59ff39af3ae123d6173ee15b092973477b9
gdulik <tomas.gdula@gmail.com>
2021-04-20 23:06:49 +0200

    Updated 12-streams/api

--- 12-streams/client/src/components/streams/StreamEdit.js
+++ 12-streams/client/src/components/streams/StreamEdit.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
 
-function StreamEdit() {
-	return <div>StreamEdit</div>;
+function StreamEdit({ match, stream, fetchStream, editStream }) {
+	useEffect(
+		() => {
+			fetchStream(match.params.id);
+		},
+		[ fetchStream, match.params.id ]
+	);
+	if (!stream) {
+		return <div>Loading...</div>;
+	}
+	return <div>{stream.title}</div>;
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+	return {
+		stream: state.streams[ownProps.match.params.id]
+	};
+};
+
+const mapDispatchToProps = { fetchStream, editStream };
+
+export default connect(mapStateToProps, mapDispatchToProps)(StreamEdit);
--- 12-streams/client/src/components/streams/StreamList.js
+++ 12-streams/client/src/components/streams/StreamList.js
@@ -1,15 +1,40 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
-function StreamList({ streams, fetchStreams }) {
+function StreamList({ isSignedIn, currentUserId, streams, fetchStreams }) {
 	useEffect(() => {
 		fetchStreams();
+		// eslint-disable-next-line
 	}, []);
+
+	const renderAdmin = (stream) => {
+		if (stream.userId === currentUserId) {
+			return (
+				<div className="right floated content">
+					<Link
+						to={`/streams/edit/${stream.id}`}
+						className="ui button primary"
+					>
+						Edit
+					</Link>
+					<Link
+						to={`/streams/delete/${stream.id}`}
+						className="ui button negative"
+					>
+						Delete
+					</Link>
+				</div>
+			);
+		}
+	};
+
 	const renderList = () => {
 		return streams.map((stream) => {
 			return (
 				<div className="item" key={stream.id}>
+					{renderAdmin(stream)}
 					<i className="large middle aligned icon camera" />
 					<div className="content">
 						{stream.title}
@@ -19,16 +44,32 @@ function StreamList({ streams, fetchStreams }) {
 			);
 		});
 	};
+
+	const renderCreate = () => {
+		if (isSignedIn) {
+			return (
+				<div style={{ textAlign: 'right' }}>
+					<Link to="/streams/new" className="ui button primary">
+						Create Stream
+					</Link>
+				</div>
+			);
+		}
+	};
+
 	return (
 		<div>
 			<h2>Streams</h2>
 			<div className="ui celled list">{renderList()}</div>
+			{renderCreate()}
 		</div>
 	);
 }
 
 const mapStateToProps = (state) => ({
-	streams: Object.values(state.streams)
+	streams: Object.values(state.streams),
+	currentUserId: state.auth.userId,
+	isSignedIn: state.auth.isSignedIn
 });
 
 const mapDispatchToProps = { fetchStreams };
